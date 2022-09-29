0c0643f3243231e8fe268559217d2b923a1c9c23
JOJO <107056624+josenankunda02@users.noreply.github.com>
2022-09-29 21:43:39 +0300

    Add files via upload

---
+++ javascript/storage.js
@@ -0,0 +1,17 @@
+
+/*function getName(){
+    var name=document.getElementById('fname').value;
+
+}
+let x=localStorage.setItem('fname', name);
+
+
+
+document.getElementById('sidename').innerHTML=localStorage.getName("fname");*/
+
+
+
+sessionStorage.setItem('fname', fname);
+var name=sessionStorage.getItem('fname')
+ 
+					
